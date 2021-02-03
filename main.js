133e48546d6ece7c534678b4f879356676568c8c
Abelrabbit19961216 <>
2021-02-03 21:44:16 +0800

    110.02.03

--- main.js
+++ main.js
@@ -7,5 +7,22 @@ $("#banner").slick({
 $("#works").slick({
     slidesToShow: 3,
     slidesToScroll:2,
+    
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 767,
+            settings :{
+                slidesToShow: 3,
+                slidesToScroll: 1 
+            }
+        }
+    ]
 });
     
